562f5aba4d96317c805baf442b8e23b43e9ce751
Lucian Branescu Mihaila <lucian.branescu@gmail.com>
2012-06-20 19:56:56 +0100

    Remove extra package.

--- airena/clocks.py
+++ airena/clocks.py
@@ -1,4 +1,3 @@
-
 class BaseClock(object):
     def __init__(self):
         pass
@@ -6,7 +5,9 @@ class BaseClock(object):
     def tick(self, framerate):
         pass
 
+
 def load_clock(class_name):
     clock_classes = load('airena.clocks', subclasses=BaseClock)
 
-    for cls in clock_classes
+    for cls in clock_classes:
+        pass
