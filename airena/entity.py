dbe920906b10d13cd582a9f35f77e42422552758
Lucian Branescu Mihaila <lucian.branescu@gmail.com>
2012-06-20 20:53:38 +0100

    Flesh out Entity and Grid.

--- airena/entity.py
+++ airena/entity.py
@@ -3,21 +3,60 @@ class Grid(object):
         self.width = width
         self.height = height
 
-        self.entities = []
+        self.entities = {}
 
-    def check_coord(self, x, y):
-        return 0 <= x < self.width and 0 <= y < self.height
+    def check(self, coords):
+        x, y = coords
+
+        if not (0 <= x < self.width and 0 <= y < self.height):
+            return False
+
+        if (x, y) in self.entities:
+            return False
+
+        return True
+
+    def __getitem__(self, key):
+        if isinstance(slice, key):
+            x1, y1 = slice.start
+            x2, y2 = slice.stop
+
+            subgrid = Grid(width=1, height=2)
+            return subgrid
+        else:
+            return self.entities[key]
 
     def register(self, entity):
-        if self.check_coord(entity.x, entity.y):
-            self.entities.append(entity)
-            entity.grid = self
+        if self.check(entity):
+            self.entities[tuple(entity)] = entity
             return True
 
         return False
 
 
+class World(object):
+    def __init__(self):
+        self.grid = Grid(10, 15)
+
+    def tick(self):
+        for coords, entity in self.grid.entities.iteritems():
+            entity.tick([])
+
+
 class Entity(object):
-    def __init__(self, x, y):
+    def __init__(self, x, y, brain):
         self.x = x
         self.y = y
+        self.brain = brain
+
+    def __iter__(self):
+        yield self.x
+        yield self.y
+
+    def tick(self, grid):
+        self.brain.tick(self, grid)
+
+
+class Brain(object):
+    def tick(self, entity, grid):
+        pass
